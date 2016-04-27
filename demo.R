cabf21e8f4cf982326c2ee9c9b6c8fe6543b366e
Bharath Venkatesh <bharath.venkatesh85@gmail.com>
2016-04-28 00:50:32 +0200

    Moved hello.R to demo.R

--- demo.R
+++ demo.R
@@ -12,15 +12,15 @@
 #   Build and Reload Package:  'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
-library(sqldf)
-library(stringr)
-library(igraph)
-library(entropy)
 library(iRefR)
 library(GEOquery)
 library(Biobase)
 library(org.Hs.eg.db)
 library(TCGAbiolinks)
+samplesList=c("TCGA-BH-A0H7-11A-13R-A089-07","TCGA-BH-A0H7-01A-13R-A056-07")
+query <- TCGAquery(samples = samplesList,platform = "AgilentG4502A_07_3",level = 3)
+TCGAdownload(query, path = "/tmp/dataBrca",samples = samplesList)
+tcgaData <- data.frame(TCGAprepare(query, dir = "/tmp/dataBrca",summarizedExperiment = F))
 gds = getGEO('GDS3837',AnnotGPL = TRUE, getGPL = TRUE,destdir = "/tmp")
 gpl = getGEO(Meta(gds)$platform,destdir = "/tmp")
 eset = GDS2eSet(gds, GPL=gpl,do.log2=FALSE)
@@ -29,10 +29,8 @@ expressionDataGEO=prepareExpressionData(eset,gpl,probeCombinerMean)
 iref_mitab=get_irefindex(tax_id="9606",data_folder="/tmp/",iref_version = "13.0")
 irefPPIGraph = loadPPIGraphIREF(iref_mitab)
 us=getUniProtToHGNCSymbolMapping();
-reactome_mitab= read.csv(textConnection(readLines(gzcon(url()))),header=FALSE,sep="\t",skip=1,stringsAsFactors= FALSE);
+reactome_mitab= downloadReactomeInteractions(data_folder="/tmp/")
 reactomePPIGraph=createIGraphObject(subset(unique(mapgraph),A!=B))
 reactome_pdata = read.csv(url('http://www.reactome.org/download/current/UniProt2Reactome_All_Levels.txt'),sep='\t',header = FALSE,skip = 1,stringsAsFactors = FALSE)
-samplesList=c("TCGA-BH-A0H7-11A-13R-A089-07","TCGA-BH-A0H7-01A-13R-A056-07")
-query <- TCGAquery(samples = samplesList,platform = "AgilentG4502A_07_3",level = 3)
-TCGAdownload(query, path = "/tmp/dataBrca",samples = samplesList)
-tcgaData <- TCGAprepare(query, dir = "/tmp/dataBrca",summarizedExperiment = F)
+reactomePathwayData=loadPathwayDataReactome(reactome_pdata,us)
+experimentDataGEO=runTestOnData(expressionDataGEO,1:60,61:120,logPairedTTestFunction)
