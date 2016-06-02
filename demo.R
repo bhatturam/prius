4d0bc14eca26d2271837935318ce9093e255fbd7
Bharath Venkatesh <bharath.venkatesh85@gmail.com>
2016-06-02 15:02:30 +0200

    Updated demo o import library

--- demo.R
+++ demo.R
@@ -2,20 +2,27 @@ library(GEOquery)
 library(Biobase)
 library(org.Hs.eg.db)
 library(TCGAbiolinks)
+library(devtools)
+install_github("bhatturam/prius")
+library(Prius)
+
+#Temporary Folder for experiments
+data_folder = tempdir();
 
 #Reactome Interactions and Pathway Data
-us=getUniProtToHGNCSymbolMapping(data_folder = "/tmp");
-mitab <- downloadReactomeInteractionsMITAB(data_folder="/tmp")
+us=getUniProtToHGNCSymbolMapping(data_folder);
+mitab <- downloadReactomeInteractionsMITAB(data_folder=data_folder)
 geneInteraction <- getGeneInteractionsFromReactomeMITAB(mitab,us)
 ppiGraph<-createIGraphObject(geneInteraction)
-reactome_pdata = downloadReactomePathways("/tmp",'HomoSapiens')
+reactome_pdata = downloadReactomePathways(data_folder,'HomoSapiens')
 reactomePathwayData=loadPathwayDataReactome(reactome_pdata,us)
 
 #Individual Analysis
+downloadPathTCGA = file.path(data_folder,"dataBrca");
 samplesList=c("TCGA-BH-A0H7-11A-13R-A089-07","TCGA-BH-A0H7-01A-13R-A056-07")
 query <- TCGAquery(samples = samplesList,platform = "AgilentG4502A_07_3",level = 3)
-TCGAdownload(query, path = "/tmp/dataBrca",samples = samplesList)
-expressionDataTCGA <- data.frame(TCGAprepare(query, dir = "/tmp/dataBrca",summarizedExperiment = F))
+TCGAdownload(query, path = downloadPathTCGA,samples = samplesList)
+expressionDataTCGA <- data.frame(TCGAprepare(query, dir = downloadPathTCGA,summarizedExperiment = F))
 experimentDataTCGA=runTestOnData(expressionDataTCGA,1,2,foldChangeFunction,list(exponent=2))
 personalizationVectorsTCGA=computePersonalizationVectors(experimentDataTCGA,ppiGraph,data.frame(foldChange=1),outdegreeNormalizedFCScoreFunction)
 pageRanksTCGA=computePageRanks(personalizationVectorsTCGA,ppiGraph,0.7)
@@ -24,8 +31,8 @@ resultTCGA=cbind(reactomePathwayData$pathwayInfo,pathwayScoresTCGA)
 resultTCGA=resultTCGA[with(resultTCGA,order(-meanAbsoluteDeviation)),]
 
 #Cohort Analysis
-gds = getGEO('GDS3837',AnnotGPL = TRUE, getGPL = TRUE,destdir = "/tmp")
-gpl = getGEO(Meta(gds)$platform,destdir = "/tmp")
+gds = getGEO('GDS3837',AnnotGPL = TRUE, getGPL = TRUE,destdir = data_folder)
+gpl = getGEO(Meta(gds)$platform,destdir = data_folder)
 eset = GDS2eSet(gds, GPL=gpl,do.log2=FALSE)
 numPairs = dim(pData(eset))[1]/2
 expressionDataGEO=importExpressionDataGEO(eset,gpl)
